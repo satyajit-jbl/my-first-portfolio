09daa2b63aac1015c9b3391748ec4e932f90c20a
satyajit <satyajit_jbl@yahoo.com>
2025-10-19 16:52:25 +0600

    local develpment bug solved

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,3 +2,4 @@ export const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api"
     : "https://my-first-portfolio-backend.vercel.app/api";
+    // : "https://my-first-portfolio-backend.vercel.app/api";
